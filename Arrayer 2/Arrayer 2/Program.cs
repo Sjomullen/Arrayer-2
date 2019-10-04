3917562574073f0aae3950f5b8c44edfe553f9dc
Sjomullen <filip.sjogren@outlook.com>
2019-10-04 09:09:32 +0200

    Del av uppgift 2

---
+++ Arrayer 2/Arrayer 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arrayer_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperature = new double[]{ 4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9 };
+            Console.WriteLine("average temperature: " + Average(temperature)); 
+        }
+        static double Average(double[] a)
+        {
+            double summa = 0;
+
+        }
+    }
+}
