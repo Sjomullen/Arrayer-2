b88204828b35c522b0f341a8ec87860edf88a51c
Sjomullen <filip.sjogren@outlook.com>
2019-10-11 08:52:42 +0200

    fixat koden

--- Arrayer 2/Arrayer 2/Program.cs
+++ Arrayer 2/Arrayer 2/Program.cs
@@ -12,7 +12,12 @@ namespace Arrayer_2
         static double Average(double[] a)
         {
             double summa = 0;
-
+            for(int i = 0; i < a.Length; i++)
+            {
+                summa += a[i];
+            }
+            double Average = summa / a.Length;
+            return (Math.Round(Average, 1));
         }
     }
 }
